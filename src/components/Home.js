f6e4aeab5607c99466aced7a03dc717ac2ec6cfe
Abhishek Vijayakumar <abhishekwhizzer@gmail.com>
2023-07-15 17:39:05 -0500

    added dummy data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { faker } from "@faker-js/faker";
+import SingleProduct from "./SingleProduct";
 
 const Home = () => {
-  return <div>Home</div>;
+  const productsArray = [...Array(20)].map((item) => ({
+    id: faker.string.uuid,
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.image.avatar(),
+  }));
+
+  console.log(productsArray);
+
+  const [products] = useState(productsArray);
+
+  return (
+    <div className="productContainer">
+      {products.map((prod) => {
+        return <SingleProduct prod={prod} />;
+      })}
+    </div>
+  );
 };
 
 export default Home;
